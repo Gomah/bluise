25c6681846bab7910a7173271540392ec1aee9e0
Gomah <hi@gomah.fr>
2019-12-10 14:55:24 +1100

    fix(netlify-form): add missing name attribute

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -4,31 +4,34 @@
       <div class="flex flex-col w-full xl:w-3/5 justify-center lg:items-start overflow-y-hidden">
         <div v-html="$md.render(welcomeText)" class="home__welcome markdown" />
 
-        <h4 v-if="isSignedUp">Thank you - we'll be in touch shortly.</h4>
+        <div class="mb-12 xl:mb-0">
+          <h4 v-if="isSignedUp">Thank you - we'll be in touch shortly.</h4>
 
-        <form
-          v-else
-          @submit.prevent="handleSubmit"
-          name="signups"
-          netlify
-          class="flex items-center border-b border-b-2 border-blue-400 py-2"
-        >
-          <input
-            ref="emailInput"
-            v-model="form.email"
-            class="appearance-none bg-transparent border-none w-full text-gray-700 mr-3 py-1 px-2 leading-tight focus:outline-none"
-            type="text"
-            placeholder="your@email.com"
-            aria-label="Email address"
-          />
-
-          <button
-            class="flex-shrink-0 bg-blue-500 hover:bg-blue-700 border-blue-500 hover:border-blue-700 text-sm border-4 text-white py-1 px-2 rounded"
-            type="submit"
+          <form
+            v-else
+            @submit.prevent="handleSubmit"
+            name="signups"
+            netlify
+            class="flex items-center border-b border-b-2 border-blue-400 py-2"
           >
-            Sign Up
-          </button>
-        </form>
+            <input
+              ref="emailInput"
+              v-model="form.email"
+              class="appearance-none mb-36 bg-transparent border-none w-full text-gray-700 mr-3 py-1 px-2 leading-tight focus:outline-none"
+              type="text"
+              name="email"
+              placeholder="your@email.com"
+              aria-label="Email address"
+            />
+
+            <button
+              class="flex-shrink-0 bg-blue-500 hover:bg-blue-700 border-blue-500 hover:border-blue-700 text-sm border-4 text-white py-1 px-2 rounded"
+              type="submit"
+            >
+              Sign Up
+            </button>
+          </form>
+        </div>
       </div>
       <div class="flex flex-col w-full xl:w-2/5">
         <img class="rounded shadow-xl" src="https://source.unsplash.com/random/720x400" />
